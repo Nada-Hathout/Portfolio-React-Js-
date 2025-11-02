089f541c093c492d817460a43b078a06c6d81662
Nada-Hathout <nhathout68@gmail.com>
2025-11-02 16:44:49 +0200

    update cv

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -11,7 +11,7 @@ function HeroSection(){
                 <p className="hero-subtitle">Full Stack Developer (.Net+Angular)</p>
             </div>
             <div className="hero-img col-md-6 img-fluid text-center slide-up">
-                <img className='hero-photo' src="/images/coding-female.png" alt="Logo" width="150" />
+                <img className='hero-photo' src="./images/coding-female.png" alt="Logo" width="150" />
             </div>
         </div>
       </div>
@@ -24,7 +24,7 @@ function HeroSection(){
             <div className="hero-img col-md-8 img-fluid text-start ">
                 <p>I’m Nada Gamal, a passionate Full Stack Web Developer with a strong focus on building modern web applications using Angular and ASP.NET Core Web API.<br />I recently graduated from the Information Technology Institute (ITI) – PD & PI track – where I developed full-featured systems following clean architecture principles. I enjoy turning complex problems into efficient and user-friendly solutions, with experience in Entity Framework, SQL Server, and Azure Blob Storage integration.I’m currently looking for opportunities to grow as a .NET Backend or Full Stack Developer, where I can apply my skills in scalable and high-performance web systems.</p>
                  <a
-      href="/Nada-Gamal-Hathout-Full-Stack.pdf"             // المسار النسبي داخل مجلد public
+      href="./Nada-Gamal-Hathout-v2FullStack.pdf"             // المسار النسبي داخل مجلد public
       download="Nada-Gamal-Hathout-Full-Stack.pdf" // الاسم اللي هيظهر عند التحميل
       className="btn btn-dark mt-3"
     >
